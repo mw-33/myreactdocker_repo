ac0d4463896f7848d479a34b17a472fb10dd3bd1
KMW <kmw8843@gmail.com>
2026-01-08 12:11:11 +0900

    fetch 추가

--- myreactdocker/src/App.jsx
+++ myreactdocker/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   const connNode = () => {
-    fetch('http://10.0.20.6:3001')
+    fetch('/api')
     .then(()=>{
       alert('success')
     })
